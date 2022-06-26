0bd4899d0660d5c7c2d093b594e691085a702dd3
VidmantasOlubas <olubasvidmantas123@gmail.com>
2022-06-26 23:25:30 +0300

    part 18

---
+++ frontend/src/components/KategorijosSelect.jsx
@@ -0,0 +1,53 @@
+import "./KategorijosSelect.css"
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+const KategorijosSelect = ({ state_status_text, set_state_kategorija }) =>
+{
+    //success loading error
+
+    const [state_status, set_state_status] = useState("loaded")
+
+    const [state_kategorijos, set_state_kategorijos] = useState([])
+
+    const handle_read = async () =>
+    {
+        try
+        {
+            set_state_status("loading")
+            const result = await axios({
+                method: "get",
+                url: "/api/kategorijos",
+                data: {}
+            })
+            set_state_status("success")
+            set_state_kategorijos(result.data)
+            set_state_kategorija(result.data[0].pavadinimas)
+        }
+        catch (err)
+        {
+            set_state_status("error")
+        }
+    }
+
+    useEffect(() =>
+    {
+        handle_read()
+    }, [])
+
+    return (
+        <div className="KategorijosSelect">
+
+            <select onChange={(param) => { set_state_kategorija(param.target.options[param.target.selectedIndex].innerHTML) }}>
+
+                {
+                    state_kategorijos.map((ele, i) => { return <option key={i}>{ele.pavadinimas}</option> })
+                }
+
+            </select>
+
+        </div>
+    )
+}
+
+export default KategorijosSelect
